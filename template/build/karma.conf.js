69ac5ee8798b41715abecd3a25f3854b603391b6
scottbedard <scottbedard90@gmail.com>
2016-02-19 00:33:49 -0700

    Fix example unit test

--- template/build/karma.conf.js
+++ template/build/karma.conf.js
@@ -1,18 +1,19 @@
-var webpackConf = require('./webpack.base.conf')
-delete webpackConf.entry
+var webpackConf = require('./webpack.base.conf');
+delete webpackConf.plugins;
+delete webpackConf.entry;
 
 module.exports = function (config) {
-  config.set({
-    browsers: ['PhantomJS'],
-    frameworks: ['jasmine'],
-    reporters: ['spec'],
-    files: ['../test/unit/index.js'],
-    preprocessors: {
-      '../test/unit/index.js': ['webpack']
-    },
-    webpack: webpackConf,
-    webpackMiddleware: {
-      noInfo: true
-    }
-  })
-}
+    config.set({
+        browsers: ['PhantomJS'],
+        frameworks: ['jasmine'],
+        reporters: ['spec'],
+        files: ['../test/unit/index.js'],
+        preprocessors: {
+            '../test/unit/index.js': ['webpack'],
+        },
+        webpack: webpackConf,
+        webpackMiddleware: {
+            noInfo: true,
+        },
+    });
+};
