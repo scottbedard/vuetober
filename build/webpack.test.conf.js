00c43558c162d29a35ccc1ff3d36d489baccebfd
Scott Bedard <scottbedard@users.noreply.github.com>
2017-02-07 15:07:09 -0700

    Include template compiler in testing environment

--- build/webpack.test.conf.js
+++ build/webpack.test.conf.js
@@ -5,15 +5,21 @@ var webpack = require('webpack');
 
 var webpackConfig = merge(baseConfig, {
     // use inline sourcemap for karma-sourcemap-loader
+    devtool: '#inline-source-map',
     module: {
         rules: utils.styleLoaders(),
     },
-    devtool: '#inline-source-map',
     plugins: [
         new webpack.DefinePlugin({
-            'process.env': require('../config/test.env')
+            'process.env': require('../config/test.env'),
         }),
     ],
+    resolve: {
+        alias: {
+            // include the template compiler in our test environment
+            'vue$': 'vue/dist/vue.common.js',
+        },
+    },        
 });
 
 // no need for app entry during tests
