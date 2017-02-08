b30d586f220c3fe20a8f5299afc7dd10b5ad2704
Scott Bedard <scottbedard90@gmail.com>
2017-02-07 17:33:27 -0700

    Add example test

--- test/unit/index.js
+++ test/unit/index.js
@@ -2,12 +2,18 @@
 /* eslint-disable no-extend-native */
 Function.prototype.bind = require('function-bind');
 
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
 // require all test files (files that ends with .spec.js)
 const testsContext = require.context('./specs', true, /\.spec$/);
 testsContext.keys().forEach(testsContext);
 
 // require all files that should be included in code coverage
 let coverageBlacklist = [
+    './app/boot.js',
     './index.htm',
     './main.js',
 ];
