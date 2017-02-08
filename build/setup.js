f1f6d4b5c2a0f6fd7e3b5aa08c3da0d1f0bce01f
scottbedard <scottbedard90@gmail.com>
2017-02-08 09:32:51 -0700

    Clean up git repository

--- build/setup.js
+++ build/setup.js
@@ -5,12 +5,7 @@ var fs = require('fs');
 var path = require('path');
 var prompt = require('prompt');
 
-prompt.message = false;
-
 const themeDirectory = path.basename(path.resolve(__dirname, '..'));
-
-console.log (colors.white('  Setup a new Vuetober theme'));
-console.log ();
 
 const schema = {
     properties: {
@@ -42,8 +37,39 @@ const schema = {
     },
 };
 
+//
+// if they haven't removed our repo yet, offer to do it for them
+//
+let isVuetoberRepository;
+try {
+    const gitConfig = fs.readFileSync(path.resolve(__dirname, '../.git/config'), 'utf8');
+    isVuetoberRepository = Boolean(gitConfig.match(/github\.com[\/:]scottbedard\/oc-vuetober-theme/g));
+} catch (err) {
+    isVuetoberRepository = false;
+}
+
+if (isVuetoberRepository) {
+    schema.properties.cleanupGit = {
+        before: value => Boolean(value.match(/^[Yy](.*)/)),
+        default: 'y',
+        description: colors.gray('  Remove git repository (y/n)'),
+        pattern: /^[YyNn](.*)*/,
+        required: true,
+    };
+}
+
+//
+// start the setup prompt
+//
+console.log (colors.white('  Setup a new Vuetober theme'));
+console.log ();
+
+prompt.message = false;
 prompt.start();
 
+//
+// process the answers the user provided
+//
 prompt.get(schema, function (err, result) {
     if (err) {
         console.log ();
@@ -61,13 +87,13 @@ prompt.get(schema, function (err, result) {
     fs.writeFileSync(path.resolve(__dirname, '../theme.yaml'), theme);
 
     // update our dev-server to use the correct url and directory
-    var devServer = fs.readFileSync(path.resolve(__dirname, './dev-server.js'), 'utf8')
+    const devServer = fs.readFileSync(path.resolve(__dirname, './dev-server.js'), 'utf8')
         .replace(/oc-vuetober-theme/g, themeDirectory)
         .replace(/http:\/\/vuetober\.october\.dev/g, result.url);
 
     fs.writeFileSync(path.resolve(__dirname, './dev-server.js'), devServer);
 
-    // remove the setup command once we're done setting up
+    // remove the setup command
     if (result.cleanup) {
         var setupPath = path.resolve(__dirname, 'setup.js');
         var packageJson = fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf8')
@@ -75,6 +101,11 @@ prompt.get(schema, function (err, result) {
 
         fs.writeFileSync(path.resolve(__dirname, '../package.json'), packageJson);
         fs.unlink(setupPath);
+    }
+
+    // remove the old git repository
+    if (result.cleanupGit) {
+        fs.unlink(path.resolve(__dirname, '../.git'));
     }
 
     console.log ();
