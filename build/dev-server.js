ba6d292fe7c9abdf9bdb03be6fd58dc82d166915
Scott Bedard <scottbedard@users.noreply.github.com>
2018-02-06 13:05:12 -0700

    fix hot reloading with wepback 3

--- build/dev-server.js
+++ build/dev-server.js
@@ -37,14 +37,6 @@ var hotMiddleware = require('webpack-hot-middleware')(compiler, {
     log: () => {},
 });
 
-// force page reload when html-webpack-plugin template changes
-compiler.plugin('compilation', function (compilation) {
-    compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-        hotMiddleware.publish({ action: 'reload' });
-        cb();
-    });
-});
-
 // proxy api requests
 Object.keys(proxyTable).forEach(function (context) {
     var options = proxyTable[context];
