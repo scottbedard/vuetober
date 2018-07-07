dfb6215a46de8b07377f35281d36aa389e06ea7a
scottbedard <scottbedard90@gmail.com>
2018-07-06 18:36:21 -0700

    scaffold theme config and index page

---
+++ generator/index.js
@@ -0,0 +1,4 @@
+module.exports = (api, options, rootOptions) => {
+    // copy all template files
+    api.render('./template');
+}
--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-module.exports = (api, options) => {
-    // ...
+module.exports = (api, projectOptions) => {
+    api.chainWebpack(webpackConfig => {
+        // modify webpack config with webpack-chain
+    });
+  
+    api.configureWebpack(webpackConfig => {
+        // modify webpack config
+        // or return object to be merged with webpack-merge
+    });
+  
+    api.registerCommand('test', args => {
+        // register `vue-cli-service test`
+    });
 }
