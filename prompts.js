9b8b148eb0258d647d14edd3adffefba327932d3
scottbedard <scottbedard90@gmail.com>
2018-09-13 21:05:54 -0700

    improve installation prompts fixes #35

--- prompts.js
+++ prompts.js
@@ -1,12 +1,24 @@
 var path = require('path');
 
-var themeDir = path.resolve(__dirname).split('\\').slice(-1).pop();
+var themeDir = path.resolve(__dirname, '../..').split('\\').slice(-1).pop();
 
 module.exports = [
     {
+        default: 'http://localhost',
         message: 'Application URL',
         name: 'server',
         type: 'input',
+        validate: function(url) {
+            if (!/^https?:\/\//.test(url)) {
+                return 'Invalid application url, make sure to include a protocol (http://example.com)';
+            }
+
+            if (!/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(url)) {
+                return 'Invalid application URL';
+            }
+
+            return true;
+        },
     },
     {
         default: themeDir,
