b15dc1c6af6475e06f3ee0824bd4c5279b09e8b6
scottbedard <scottbedard90@gmail.com>
2018-09-16 13:05:55 -0700

    display october errors, and improve messaging for a missing entry point closes #37

--- generator/ts/src/dev.ts
+++ generator/ts/src/dev.ts
@@ -1,9 +1,20 @@
 // fetch server data
 fetch(`<%= options.server %>${window.location.pathname}`, { mode: 'cors' })
-    .then(response => response.text())
-    .then(html => {
+    .then(response => new Promise(resolve => response.text().then(html => resolve({ html, response }))))
+    .then(({ html, response }) => {
         const parser = new DOMParser();
         const doc = parser.parseFromString(html, "text/html");
+
+        // display october errors
+        if (response.status >= 400) {
+            document.open('text/html');
+            document.write(html);
+            document.close();
+
+            console.error(response);
+
+            return;
+        }
 
         // append the vuetober meta tag to the <head>, and start the application
         const vuetober = doc.querySelector('meta[name=vuetober]');
