1e5ffdcfd189df2406f4312854623381231bd7f8
scottbedard <scottbedard90@gmail.com>
2018-09-16 18:41:41 -0700

    fix session/proxying problem, and dramatically simplify entry point closes #40

--- generator/index.js
+++ generator/index.js
@@ -1,14 +1,3 @@
 module.exports = (api, options, rootOptions) => {
-    // render common files
-    api.render('./common', { options });
-
-    // typescript
-    if (api.hasPlugin('typescript')) {
-        api.render('./ts', { options });
-    }
-
-    // javascript
-    else {
-        api.render('./js', { options });
-    }
+    api.render('./template', { options });
 }
--- index.js
+++ index.js
@@ -1,40 +1,49 @@
 var path = require('path');
-
-var isProduction = process.env.NODE_ENV === 'production';
-var themeDir = path.resolve(__dirname, '../..').split('\\').slice(-1).pop();
+var WriteFilePlugin = require('write-file-webpack-plugin');
 
 module.exports = (api, options) => {
+    var isProduction = process.env.NODE_ENV === 'production';
+    var themeDir = path.resolve(__dirname, '../..').split('\\').slice(-1).pop();
+
+    // use dev assets when not in production
+    options.baseUrl = isProduction 
+        ? '/themes/' + themeDir + '/assets/' 
+        : 'http://localhost:8080/';
+
     // set webpack's output directory to /assets
     options.outputDir = 'assets';
-    
-    //
-    // production
-    //
-    if (process.env.NODE_ENV === 'production') {
-        // in production reference assets from theme directory
-        options.baseUrl = '/themes/' + themeDir + '/assets/';
 
-        // save our only page to /pages
-        api.chainWebpack(function(config) {
-            config.plugin('html').tap(function(args) {
-                Object.assign(args[0], {
-                    filename: '../pages/index.htm',
-                    template: 'src/index.htm',
-                });
-
-                return args;
+    // save our only page to /pages
+    api.chainWebpack(function(config) {
+        config.plugin('html').tap(function(args) {
+            Object.assign(args[0], {
+                minify: Object.assign((args[0].minify || {}), {
+                    // comments will always need to be removed to preserve october's
+                    // theme configuration and php section at the top of the file.
+                    removeComments: true,
+                }),
+                filename: '../pages/index.htm',
+                template: 'src/index.htm',
             });
+
+            return args;
         });
-    }
+    });
 
     //
     // non-production
     //
-    else {
-        // use our dev entry point
+    if (!isProduction) {
         api.configureWebpack(function(config) {
-            config.entry = { 
-                app: ['./src/dev'],
+            return {
+                devServer: {
+                    headers: {
+                        'Access-Control-Allow-Origin': '*',
+                    },
+                },
+                plugins: [
+                    new WriteFilePlugin({ test: /\.htm$/ }),
+                ],
             };
         });
     }
